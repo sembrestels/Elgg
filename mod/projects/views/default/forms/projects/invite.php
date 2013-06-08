c6fbebc81117ffb8cc4a17e9420d074b2227485e
Sem <sembrestels@riseup.net>
2013-06-08 06:59:21 +0200

    No friends requirement to add people to a project.

--- mod/projects/views/default/forms/projects/invite.php
+++ mod/projects/views/default/forms/projects/invite.php
@@ -8,15 +8,9 @@
 
 $project = $vars['entity'];
 $forward_url = $project->getURL();
-$friends = elgg_get_logged_in_user_entity()->getFriends('', 0);
-
-if ($friends) {
-	echo elgg_view('input/userpicker', array('name' => 'user_guid'));
-	echo '<div class="elgg-foot">';
-	echo elgg_view('input/hidden', array('name' => 'forward_url', 'value' => $forward_url));
-	echo elgg_view('input/hidden', array('name' => 'project_guid', 'value' => $project->guid));
-	echo elgg_view('input/submit', array('value' => elgg_echo('add')));
-	echo '</div>';
-} else {
-	echo elgg_echo('projects:nofriendsatall');
-}
+echo elgg_view('input/userpicker', array('name' => 'user_guid'));
+echo '<div class="elgg-foot">';
+echo elgg_view('input/hidden', array('name' => 'forward_url', 'value' => $forward_url));
+echo elgg_view('input/hidden', array('name' => 'project_guid', 'value' => $project->guid));
+echo elgg_view('input/submit', array('value' => elgg_echo('add')));
+echo '</div>';
