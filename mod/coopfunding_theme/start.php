dcf9454b6a2913f4128eb4d20d944cfee1cc809e
Sembrestels <sembrestels@riseup.net>
2013-06-23 14:31:16 +0200

    Removed elgg logo from topbar.

--- mod/coopfunding_theme/start.php
+++ mod/coopfunding_theme/start.php
@@ -5,16 +5,16 @@
  * @package Coopfunding
  * @subpackage Theme
  */
- 
+
 elgg_register_event_handler('init','system','coopfunding_theme_init');
- 
+
 function coopfunding_theme_init() {
 
 	elgg_register_event_handler('pagesetup', 'system', function() {
 
 		elgg_unregister_menu_item('footer', 'elgg');
-		
-		if(elgg_is_active_plugin('externalpages')) {
+		elgg_unregister_menu_item('topbar', 'elgg_logo');
+		if (elgg_is_active_plugin('externalpages')) {
 			elgg_register_menu_item('site', array(
 				'name' => 'about',
 				'href' => 'about',
